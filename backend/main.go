63bdc37ac2a7d32dda9a83c91ebb3b337481a03e
RodrigoCF25 <rodrigocf88@hotmail.com>
2025-06-03 15:28:19 -0600

    Commented some endpoints that are not needed for this project scope

--- backend/main.go
+++ backend/main.go
@@ -42,24 +42,23 @@ func main() {
 
 func BindRoutes(s server.Server, r *mux.Router) error {
 	r.Use(middleware.CheckAuthMiddleware(s))
-	r.HandleFunc("/", handlers.HomeHanlder(s)).Methods("GET")
 
 	//user
-	r.HandleFunc("/signup", handlers.SignUpHandler(s)).Methods("POST")
-	r.HandleFunc("/login", handlers.LoginHandler(s)).Methods("POST")
-	r.HandleFunc("/auth/google", handlers.GoogleLoginHandler(s)).Methods("POST")
-	r.HandleFunc("/me", handlers.MeHandler(s)).Methods("GET")
+	r.HandleFunc("/signup", handlers.SignUpHandler(s)).Methods("POST")           //registro
+	r.HandleFunc("/login", handlers.LoginHandler(s)).Methods("POST")             //inicio de sesión por correo y contraseña
+	r.HandleFunc("/auth/google", handlers.GoogleLoginHandler(s)).Methods("POST") //inicio de sesión por Google
+	//r.HandleFunc("/me", handlers.MeHandler(s)).Methods("GET")					//obtener información de un usuario
 
 	//products
-	r.HandleFunc("/products", handlers.InsertProductHandler(s)).Methods("POST")
-	r.HandleFunc("/products/{id}", handlers.GetProductById(s)).Methods("GET")
-	r.HandleFunc("/products/{id}", handlers.UpdateProduct(s)).Methods("PUT")
-	r.HandleFunc("/products", handlers.ListProduct(s)).Methods("GET")
+	//r.HandleFunc("/products", handlers.InsertProductHandler(s)).Methods("POST")	//añadir producto
+	r.HandleFunc("/products/{id}", handlers.GetProductById(s)).Methods("GET") //obtener un producto
+	//r.HandleFunc("/products/{id}", handlers.UpdateProduct(s)).Methods("PUT")  //actualizar un producto
+	r.HandleFunc("/products", handlers.ListProduct(s)).Methods("GET") //obtener todos los productos
 
 	//cart
 	r.HandleFunc("/cart", handlers.GetCartHandler(s)).Methods("GET")                    // obtener carrito
 	r.HandleFunc("/cart/item", handlers.UpsertCartItemHandler(s)).Methods("PUT")        // actualizar cantidad (añadir producto y su cantidad)
-	r.HandleFunc("/cart/item", handlers.RemoveItemFromCartHandler(s)).Methods("DELETE") // quitar un item
+	r.HandleFunc("/cart/item", handlers.RemoveItemFromCartHandler(s)).Methods("DELETE") // quitar un item del carrito
 	r.HandleFunc("/cart", handlers.ClearCartHandler(s)).Methods("DELETE")               // limpiar carrito
 
 	return nil
